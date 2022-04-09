5fbf0b09952710602f90065ff8cc22cb8e750c94
Kyle McCandless <43144919+KMcCand@users.noreply.github.com>
2022-04-08 22:27:49 -0700

    Update BurgerQueensV1.py
    add code to import test csv

--- BurgerQueensV1.py
+++ BurgerQueensV1.py
@@ -3,10 +3,13 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
 train_csv = pd.read_csv('/groups/CS156b/data/student_labels/train.csv')
-for row in train_csv:
+for row in range(len(train_csv)):
     path = '/groups/CS156b/data/' + train_csv['Path'][row]
     image = mpimg.imread(path)
-#     plt.show()
-#     plt.imshow(image)
+    
+test_csv = pd.read_csv('/groups/CS156b/data/student_labels/test.csv')
+for row in range(len(test_csv)):
+    path = '/groups/CS156b/data/' + test_csv['Path'][row]
+    image = mpimg.imread(path)
 # test_csv = pd.read_csv('/groups/CS156b/data/test')
 # solution_csv = pd.read_csv('/groups/CS156b/data/solution')
