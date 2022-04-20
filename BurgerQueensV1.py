f69baa9f6b7c59f153c8ef79947ef95b5366ed88
Kyle McCandless <43144919+KMcCand@users.noreply.github.com>
2022-04-19 19:46:38 -0700

    Update BurgerQueensV1.py
    remove test.csv because it was incorrect filename, only output 100 images

--- BurgerQueensV1.py
+++ BurgerQueensV1.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+%matplotlib inline
 
 train_csv = pd.read_csv('/groups/CS156b/data/student_labels/train.csv')
-for row in range(len(train_csv)):
+for row in range(1, 100):
     path = '/groups/CS156b/data/' + train_csv['Path'][row]
     image = mpimg.imread(path)
+    print(train_csv['Path'][row])
+    plt.imshow(image)
+    plt.show()
     
-test_csv = pd.read_csv('/groups/CS156b/data/student_labels/test.csv')
-for row in range(len(test_csv)):
-    path = '/groups/CS156b/data/' + test_csv['Path'][row]
-    image = mpimg.imread(path)
-# test_csv = pd.read_csv('/groups/CS156b/data/test')
-# solution_csv = pd.read_csv('/groups/CS156b/data/solution')
